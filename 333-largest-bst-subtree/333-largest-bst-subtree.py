95fc5114fabd4f3a0d9ae14fede507902b51512b
Di Bu <dibu@andrew.cmu.edu>
2022-09-20 00:31:57 -0700

    Time: 115 ms (17.24%), Space: 16.6 MB (72.62%) - LeetHub

--- 333-largest-bst-subtree/333-largest-bst-subtree.py
+++ 333-largest-bst-subtree/333-largest-bst-subtree.py
@@ -4,14 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# class SubTree(object):
+#     def __init__(self, largest, n, min, max):
+#         self.largest = largest
+#         self.n = n
+#         self.min = min
+#         self.max = max
+
 class Solution:
     def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
+        
         def dfs(root):
             if not root:
                 return 0, 0, float('inf'), float('-inf')
             N1, n1, min1, max1 = dfs(root.left)
             N2, n2, min2, max2 = dfs(root.right)
-            n = n1 + n2 + 1 if max1 < root.val < min2 else float('-inf')
+            n = n1 + 1 + n2 if max1 < root.val < min2 else float('-inf')
             return max(N1, N2, n), n, min(min1, root.val), max(max2, root.val)
         
         return dfs(root)[0]
+    
+#     def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
+#         res = self.dfs(root)
+#         return res.largest
+    
+#     def dfs(self, root):
+#         if not root:
+#             return SubTree(0, 0, float('inf'), float('-inf'))
+        
+#         left = self.dfs(root.left)
+#         right = self.dfs(root.right)
+        
+#         if root.val > left.max and root.val < right.min:
+#             n = left.n + right.n + 1
+#         else:
+#             n = float('-inf')
+        
+#         largest = max(left.largest, right.largest, n)
+#         return SubTree(largest, n, min(left.min, root.val), max(right.max, root.val))
