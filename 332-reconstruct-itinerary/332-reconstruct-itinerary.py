4143bf555be85385fe0d496fdc143349eb08c391
Di Bu <dibu@andrew.cmu.edu>
2022-09-20 00:22:59 -0700

    Time: 111 ms (73.87%), Space: 14.4 MB (74.90%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        targets = defaultdict(list)
+        for a, b in sorted(tickets)[::-1]:
+            targets[a] += b,
+        route = []
+        
+        def visit(airport):
+            while targets[airport]:
+                visit(targets[airport].pop())
+            route.append(airport)
+        
+        visit('JFK')
+        
+        return route[::-1]
