5924a2481d98bad9e81b207ce091ee0d458d8b41
Di Bu <dibu@andrew.cmu.edu>
2022-09-19 21:22:46 -0700

    Time: 109 ms (51.05%), Space: 14.3 MB (44.80%) - LeetHub

--- 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
@@ -1,14 +1,26 @@
 class Solution:
     def multiply(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
         m, n, l = len(A), len(A[0]), len(B[0])
-        C = [[0 for _ in range(l)] for _ in range(m)]
-        tableB = {}
-        for k, row in enumerate(B):
-            tableB[k] = {}
-            for j, eleB in enumerate(row):
-                if eleB: tableB[k][j] = eleB
+        table_A, table_B = {}, {}
+        
         for i, row in enumerate(A):
-            for k, eleA in enumerate(row):
-                for j, eleB in tableB[k].items():
-                    C[i][j] += eleA * eleB
+            for j, ele in enumerate(row):
+                if ele:
+                    if i not in table_A: table_A[i] = {}
+                    table_A[i][j] = ele
+        
+        for i, row in enumerate(B):
+            for j, ele in enumerate(row):
+                if ele:
+                    if i not in table_B: table_B[i] = {}
+                    table_B[i][j] = ele
+        
+        C = [[0 for j in range(l)] for i in range(m)]
+        for i in table_A:
+            for k in table_A[i]:
+                if k not in table_B: continue
+                for j in table_B[k]:
+                    C[i][j] += table_A[i][k] * table_B[k][j]
+        
         return C
+            
