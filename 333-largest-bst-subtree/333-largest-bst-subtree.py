d89191b77c689551ba7a64ccfaa201f00c688554
Di Bu <dibu@andrew.cmu.edu>
2022-09-20 00:27:29 -0700

    Time: 71 ms (68.54%), Space: 16.9 MB (23.43%) - LeetHub

---
+++ 333-largest-bst-subtree/333-largest-bst-subtree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+                return 0, 0, float('inf'), float('-inf')
+            N1, n1, min1, max1 = dfs(root.left)
+            N2, n2, min2, max2 = dfs(root.right)
+            n = n1 + n2 + 1 if max1 < root.val < min2 else float('-inf')
+            return max(N1, N2, n), n, min(min1, root.val), max(max2, root.val)
+        
+        return dfs(root)[0]
