49b0d9081f193da4806a8c1ca63f676045002496
Di Bu <dibu@andrew.cmu.edu>
2022-09-20 11:38:33 -0700

    Time: 82 ms (50.31%), Space: 14 MB (91.32%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,8 +1,12 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # Brute-Force Approach
+        # Dict Approach (record all nums appeared in one list)
         res = []
+        dict = {}
         for i in nums1:
-            if i not in res and i in nums2:
-                res.append(i)
+            dict[i] = dict[i] + 1 if i in dict else 1
+        for j in nums2:
+            if j in dict and dict[j] > 0:
+                res.append(j)
+                dict[j] = 0
         return res
