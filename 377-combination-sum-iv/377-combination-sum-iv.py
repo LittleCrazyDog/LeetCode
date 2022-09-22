295f44e29868abb28a17aff38e05848db43ab886
Di Bu <dibu@andrew.cmu.edu>
2022-09-22 11:42:14 -0700

    Time: 88 ms (12.67%), Space: 13.9 MB (81.64%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -0,0 +1,9 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        nums, combs = sorted(nums), [1] + [0] * (target)
+        for i in range(target+1):
+            for num in nums:
+                if num > i: break
+                if num == i: combs[i] += 1
+                if num < i: combs[i] += combs[i-num]
+        return combs[target]
