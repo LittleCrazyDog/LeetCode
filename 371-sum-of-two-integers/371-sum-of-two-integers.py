bf26b2ffcead8bf56fda8e9e8ae3af539ae9f8c3
Di Bu <dibu@andrew.cmu.edu>
2022-09-22 10:50:00 -0700

    Time: 64 ms (9.57%), Space: 13.8 MB (96.69%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # 32 bits integer min
+        MIN = 0x80000000
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
+        while b != 0:
+            # ^ get different bits and & get double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
+        return a if a <= MAX else ~(a ^ mask)
