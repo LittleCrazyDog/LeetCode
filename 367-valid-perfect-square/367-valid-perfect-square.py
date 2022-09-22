f1c5378a08d24d14d680bde0cba0ba6dad39b63a
Di Bu <dibu@andrew.cmu.edu>
2022-09-22 10:08:39 -0700

    Time: 99 ms (6.04%), Space: 13.8 MB (55.30%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -1,7 +1,8 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        # Newton's Method
-        r = num
-        while r * r > num:
-            r = (r + num/r) // 2
-        return r * r == num
+        # Math Trick for Square Number is 1 + 3 + 5 + ... + (2n-1)
+        i = 1
+        while num > 0:
+            num -= i
+            i += 2
+        return num == 0
