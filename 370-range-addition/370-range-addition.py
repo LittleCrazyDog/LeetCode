bc12e11ce6cd9d168a3937536324b1cee8fc5564
Di Bu <dibu@andrew.cmu.edu>
2022-09-22 10:37:32 -0700

    Time: 599 ms (5.13%), Space: 22.6 MB (87.61%) - LeetHub

---
+++ 370-range-addition/370-range-addition.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
+        ans = [0] * length
+        
+        for start, end, value in updates:
+            ans[start] += value
+            end += 1
+            if end < len(ans):
+                ans[end] -= value
+        
+        for i in range(1, len(ans)):
+            ans[i] += ans[i-1]
+        
+        return ans
