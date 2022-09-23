ecdc968770b693abf2e194304960538e4c7f9871
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 10:34:10 -0700

    Time: 90 ms (5.21%), Space: 13.9 MB (22.54%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,4 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        odds = sum(v & 1 for v in Counter(s).values())
-        return len(s) - odds + bool(odds)
+        # Clean Code
+        # odds = sum(v & 1 for v in Counter(s).values())
+        # return len(s) - odds + bool(odds)
+        ctr = defaultdict(int)
+        for ch in s:
+            ctr[ch] += 1
+        ret, odd_ctr, sum_odd = 0, 0, 0
+        for ch, freq in ctr.items():
+            if freq % 2 == 0:
+                ret += freq
+            else:
+                odd_ctr += 1
+                sum_odd += freq
+        if odd_ctr > 0:
+            ret = ret + sum_odd - (odd_ctr - 1)
+        return ret
