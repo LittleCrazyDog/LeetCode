65fc9297fad8afb1964c7e13e8124ec05147dbd4
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 11:12:36 -0700

    Time: 84 ms (9.75%), Space: 14.2 MB (12.05%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -2,11 +2,24 @@ class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         # Bottom Up DP
         # Let dp[i] denote the number of arithmetic subarray ends at nums[i]
+        # n = len(nums)
+        # dp = [0] * n
+        # ans = 0
+        # for i in range(2, n):
+        #     if nums[i-1] - nums[i-2] == nums[i] - nums[i-1]:
+        #         dp[i] = dp[i-1] + 1
+        #     ans += dp[i]
+        # return ans
+        
+        
+        # Bottom Up DP (Space Optimized)
         n = len(nums)
-        dp = [0] * n
+        dp, dpPrev = 0, 0
         ans = 0
         for i in range(2, n):
             if nums[i-1] - nums[i-2] == nums[i] - nums[i-1]:
-                dp[i] = dp[i-1] + 1
-            ans += dp[i]
+                dp = dpPrev + 1
+            ans += dp
+            dpPrev = dp
+            dp = 0
         return ans
