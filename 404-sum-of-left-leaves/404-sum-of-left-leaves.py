9810ba124ee8f8fa9e4c8f8dba9cfe559503f41c
Di Bu <dibu@andrew.cmu.edu>
2022-09-22 22:55:46 -0700

    Time: 66 ms (22.56%), Space: 14.2 MB (90.60%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.py
@@ -15,14 +15,27 @@ class Solution:
     #     return dfs(root, False)
     
     # Iterative DFS
+    # def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+    #     s, ans = deque([(root, False)]), 0
+    #     while s:
+    #         cur, isLeft = s.pop()
+    #         if not cur.left and not cur.right and isLeft:
+    #             ans += cur.val
+    #         if cur.right:
+    #             s.append((cur.right, False))
+    #         if cur.left:
+    #             s.append((cur.left, True))
+    #     return ans
+    
+    # BFS
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        s, ans = deque([(root, False)]), 0
-        while s:
-            cur, isLeft = s.pop()
+        q, ans = deque([(root, False)]), 0
+        while q:
+            cur, isLeft = q.popleft()
             if not cur.left and not cur.right and isLeft:
                 ans += cur.val
             if cur.right:
-                s.append((cur.right, False))
+                q.append((cur.right, False))
             if cur.left:
-                s.append((cur.left, True))
+                q.append((cur.left, True))
         return ans
