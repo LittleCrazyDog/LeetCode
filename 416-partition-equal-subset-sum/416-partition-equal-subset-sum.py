856fcc69964c1f8be911c00a081f67622d491312
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 11:47:30 -0700

    Time: 82 ms (98.75%), Space: 13.9 MB (97.24%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -8,11 +8,18 @@ class Solution:
         # total_sum = sum(nums)
         # return total_sum & 1 == 0 and subsetSum(total_sum // 2, 0)
         
+        # total_sum = sum(nums)
+        # if total_sum & 1: return False
+        # half_sum = total_sum // 2
+        # dp = [True] + [False] * half_sum
+        # for num in nums:
+        #     for j in range(half_sum, num-1, -1):
+        #         dp[j] |= dp[j-num]
+        # return dp[half_sum]
+        
         total_sum = sum(nums)
         if total_sum & 1: return False
-        half_sum = total_sum // 2
-        dp = [True] + [False] * half_sum
+        half_sum, dp = total_sum // 2, 1
         for num in nums:
-            for j in range(half_sum, num-1, -1):
-                dp[j] |= dp[j-num]
-        return dp[half_sum]
+            dp |= dp << num
+        return dp & 1 << half_sum
