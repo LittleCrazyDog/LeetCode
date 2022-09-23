a023d8592ef20b785d9c8432ce9212da1d46769f
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 11:20:48 -0700

    Time: 44 ms (88.19%), Space: 14.4 MB (17.68%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -2,13 +2,14 @@ class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         num1, num2 = list(num1), list(num2)
         carry, res = 0, []
+        
         while len(num2) > 0 or len(num1) > 0:
-            n1 = ord(num1.pop()) - ord('0') if len(num1) > 0 else 0
-            n2 = ord(num2.pop()) - ord('0') if len(num2) > 0 else 0
-            
-            temp = n1 + n2 + carry
-            res.append(temp % 10)
-            carry = temp // 10
+            n1 = n2 = 0
+            if num1: n1 = ord(num1.pop()) - ord('0')
+            if num2: n2 = ord(num2.pop()) - ord('0')
+
+            carry, remain = divmod(n1 + n2 + carry, 10)
+            res.append(remain)
         
         if carry: res.append(carry)
         return ''.join([str(i) for i in res])[::-1]
