9e04cdff70c2bb349599dde27316c4a6949af765
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 22:44:23 -0700

    Time: 511 ms (23.16%), Space: 15.1 MB (82.03%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,15 +1,25 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        pC = Counter(p)
-        res = []
-        window = None
-        for i in range(len(s)-len(p)+1):
-            if i == 0:
-                window = Counter(s[i:i+len(p)])
-            else:
-                minus = {s[i-1]:1}
-                plus = {s[i+len(p)-1]:1}
-                window -= minus
-                window += plus
-            if pC == window: res.append(i)
+        hm, res, pL, sL = defaultdict(int), [], len(p), len(s)
+        if pL > sL: return []
+        
+        # build hashmap
+        for ch in p:
+            hm[ch] += 1
+            
+        # initail full pass over the window
+        for i in range(pL-1):
+            if s[i] in hm: hm[s[i]] -= 1
+        
+        # slide the window with stride 1:
+        for i in range(-1, sL-pL):
+            if i > -1 and s[i] in hm:
+                hm[s[i]] += 1
+            if i+pL < sL and s[i+pL] in hm:
+                hm[s[i+pL]] -= 1
+            
+            # check whether we encountered an anagram
+            if all(v == 0 for v in hm.values()):
+                res.append(i+1)
+        
         return res
