33d3474bead922c3d22c7b7e90aeec4ea003ffbf
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 22:14:58 -0700

    Time: 108 ms (56.89%), Space: 15.2 MB (81.65%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.py
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -5,33 +5,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Brute-Force
+    # Memorization of path sum
     def pathSum(self, root: Optional[TreeNode], target: int) -> int:
-        # define global return var
-        self.numOfPaths = 0
-        # 1st layer DFS to go through each node
-        self.dfs(root, target)
-        # return result
-        return self.numOfPaths
-    
-    # define: traverse through the tree, at each treenode, call another DFS to test if a path sum include the answer
-    def dfs(self, node, target):
-        # exit condition
-        if node is None:
-            return
-        # dfs back down
-        self.test(node, target)
-        self.dfs(node.left, target)
-        self.dfs(node.right, target)
-    
-    # define: for a given node, DFS to find any path that sum == target, if find self.numOfPaths += 1
-    def test(self, node, target):
-        # exit condition
-        if node is None:
-            return
-        if node.val == target:
-            self.numOfPaths += 1
+        # define global result and path
+        self.result = 0
+        cache = {0: 1}
         
-        # test back down
-        self.test(node.left, target-node.val)
-        self.test(node.right, target-node.val)
+        # recursive to get result
+        self.dfs(root, target, 0, cache)
+        
+        # return result
+        return self.result
+    
+    def dfs(self, root, target, currPathSum, cache):
+        # exit condition
+        if root is None:
+            return
+        
+        # calculate currPathSum and required oldPathSum
+        currPathSum += root.val
+        oldPathSum = currPathSum - target
+        
+        # update result and cache
+        self.result += cache.get(oldPathSum, 0)
+        cache[currPathSum] = cache.get(currPathSum, 0) + 1
+        
+        # dfs breakdown
+        self.dfs(root.left, target, currPathSum, cache)
+        self.dfs(root.right, target, currPathSum, cache)
+        
+        # when move to a different branch, the currPathSum is no longer available, hence remove one.
+        cache[currPathSum] -= 1
