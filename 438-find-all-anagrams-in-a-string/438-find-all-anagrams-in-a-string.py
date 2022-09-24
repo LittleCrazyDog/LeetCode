b370843c1674747167f0f954120fcdd03aad3d64
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 22:34:45 -0700

    Time: 874 ms (9.57%), Space: 15 MB (99.37%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,15 +1,15 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        pC = Counter(p)
         res = []
-        pCounter = Counter(p)
-        sCounter = Counter(s[:len(p)-1])
-        
-        for i in range(len(p)-1, len(s)):
-            sCounter[s[i]] += 1             # include a new char in the window
-            if sCounter == pCounter:        # This step is O(1), since there are at most 26 English letters
-                res.append(i-len(p)+1)      # append the starting index
-            sCounter[s[i-len(p)+1]] -= 1    # decrease the count of oldest char in the window
-            if sCounter[s[i-len(p)+1]] == 0:
-                del sCounter[s[i-len(p)+1]] # remove the count if it is 0
-        
+        window = None
+        for i in range(len(s)-len(p)+1):
+            if i == 0:
+                window = Counter(s[i:i+len(p)])
+            else:
+                minus = {s[i-1]:1}
+                plus = {s[i+len(p)-1]:1}
+                window -= minus
+                window += plus
+            if pC == window: res.append(i)
         return res
