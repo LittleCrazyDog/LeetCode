258cc66e2555f86176976ab8bcd2bae5ec740355
Di Bu <dibu@andrew.cmu.edu>
2022-09-24 00:26:38 -0700

    Time: 151 ms (22.91%), Space: 13.8 MB (87.99%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.py
@@ -6,22 +6,16 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         st1, st2 = [], []
-        
         while l1:
             st1.append(l1.val)
             l1 = l1.next
         while l2:
             st2.append(l2.val)
             l2 = l2.next
-        
         carry, head = 0, None
-        
         while st1 or st2 or carry:
             d1 = st1.pop() if st1 else 0
             d2 = st2.pop() if st2 else 0
-            carry, digit = divmod(d1 + d2 + carry, 10)
-            head_new = ListNode(digit)
-            head_new.next = head
-            head = head_new
-        
+            carry, remainder = divmod(d1 + d2 + carry, 10)
+            head = ListNode(remainder, head)
         return head
