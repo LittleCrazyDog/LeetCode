fd0cf96a608cc6a4f9cbd515983d4d77c28952ed
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 21:50:48 -0700

    Time: 831 ms (18.25%), Space: 20 MB (86.62%) - LeetHub

---
+++ 436-find-right-interval/436-find-right-interval.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        l = sorted((e[0], i) for i, e in enumerate(intervals))
+        res = []
+        for e in intervals:
+            r = bisect.bisect_left(l, (e[1],))
+            res.append(l[r][1] if r < len(l) else -1)
+        
+        return res
