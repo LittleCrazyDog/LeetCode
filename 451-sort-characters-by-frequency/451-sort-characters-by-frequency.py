2ef6699d937b693e24b83b59a9e6c327f35e2fc0
Di Bu <dibu@andrew.cmu.edu>
2022-09-24 11:10:54 -0700

    Time: 140 ms (12.77%), Space: 23.7 MB (12.99%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -9,10 +9,27 @@ class Solution:
         # return ''.join(s)
         
         # Counter & Sorting Distinct Characters
+        # Time: O(N+KlogK)
+        # Space: O(N)
+        # cnt = Counter(s)
+        # arr = [[freq, c] for c, freq in cnt.items()]
+        # arr.sort(key = lambda x: -x[0])
+        # res = []
+        # for freq, c in arr:
+        #     res.append(freq*c)
+        # return ''.join(res)
+        
+        # Counter & Bucket Sort
+        # Time: O(N)
+        # Space: O(N)
         cnt = Counter(s)
-        arr = [[freq, c] for c, freq in cnt.items()]
-        arr.sort(key = lambda x: -x[0])
+        n = len(s)
+        bucket = [[] for _ in range(n+1)]
+        for c, freq in cnt.items():
+            bucket[freq].append(c)
+        
         res = []
-        for freq, c in arr:
-            res.append(freq*c)
+        for freq in range(n, -1, -1):
+            for c in bucket[freq]:
+                res.append(c * freq)
         return ''.join(res)
