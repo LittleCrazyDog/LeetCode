79fa70c70afb31415b18a58778d9449bb0ab669c
Di Bu <dibu@andrew.cmu.edu>
2022-09-23 22:32:14 -0700

    Time: 1085 ms (6.81%), Space: 15.2 MB (35.10%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,24 +1,15 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        hm, res, pL, sL = defaultdict(int), [], len(p), len(s)
-        if pL > sL: return []
+        res = []
+        pCounter = Counter(p)
+        sCounter = Counter(s[:len(p)-1])
         
-        # build hashmap
-        for ch in p: hm[ch] += 1
-            
-        # initial full pass over the window
-        for i in range(pL-1):
-            if s[i] in hm: hm[s[i]] -= 1
-        
-        # slide the window with stride 1
-        for i in range(-1, sL-pL+1):
-            if i > -1 and s[i] in hm:
-                hm[s[i]] += 1
-            if i+pL < sL and s[i+pL] in hm:
-                hm[s[i+pL]] -= 1
-            
-            # check whether we encountered an anagram
-            if all(v == 0 for v in hm.values()):
-                res.append(i+1)
+        for i in range(len(p)-1, len(s)):
+            sCounter[s[i]] += 1             # include a new char in the window
+            if sCounter == pCounter:        # This step is O(1), since there are at most 26 English letters
+                res.append(i-len(p)+1)      # append the starting index
+            sCounter[s[i-len(p)+1]] -= 1    # decrease the count of oldest char in the window
+            if sCounter[s[i-len(p)+1]] == 0:
+                del sCounter[s[i-len(p)+1]] # remove the count if it is 0
         
         return res
