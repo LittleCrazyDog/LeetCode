7be23c42328c0d8a693626e074faf1b50b2a0d44
Di Bu <dibu@andrew.cmu.edu>
2022-09-24 18:19:20 -0700

    Time: 703 ms (30.92%), Space: 17.6 MB (70.56%) - LeetHub

--- 475-heaters/475-heaters.py
+++ 475-heaters/475-heaters.py
@@ -1,5 +1,8 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        # Go through houses and haters in ascending order. My i points to the current
+        # closest heater. Go to the next heater if the current house coordinate is larger than
+        # or equal to the middle between the current and the next heater
         heaters = sorted(heaters) + [float('inf')]
         i = r = 0
         for x in sorted(houses):
