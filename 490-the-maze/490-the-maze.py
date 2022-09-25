2b8185431fc9b22cd3bcb3ee442e3922ec978727
Di Bu <dibu@andrew.cmu.edu>
2022-09-24 23:10:05 -0700

    Time: 592 ms (40.78%), Space: 16.6 MB (15.38%) - LeetHub

--- 490-the-maze/490-the-maze.py
+++ 490-the-maze/490-the-maze.py
@@ -1,27 +1,19 @@
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
-        Q = deque([start])
-        n = len(maze)
-        m = len(maze[0])
-        dirs = ((0, 1), (0, -1), (1, 0), (-1, 0))
-        
-        maze[start[0]][start[1]] = 2
-        while Q:
-            i, j = Q.popleft()
-            
-            if i == destination[0] and j == destination[1]:
+        m, n, stopped = len(maze), len(maze[0]), set()
+        def dfs(x, y):
+            if (x, y) in stopped:
+                return False
+            stopped.add((x,y))
+            if [x,y] == destination:
                 return True
-            
-            for x, y in dirs:
-                row = i + x
-                col = j + y
-                while 0 <= row < n and 0 <= col < m and maze[row][col] != 1:
-                    row += x
-                    col += y
-                row -= x
-                col -= y
-                if maze[row][col] == 0:
-                    Q.append([row, col])
-                    maze[i][j] = 2
+            for i, j in (-1,0), (1,0), (0,-1), (0,1):
+                newX, newY = x, y
+                while 0 <= newX+i < m and 0 <= newY+j < n and maze[newX+i][newY+j] != 1:
+                    newX += i
+                    newY += j
+                if dfs(newX, newY):
+                    return True
+            return False
         
-        return False
+        return dfs(*start)
