db9daf1be046a82ec1bc46da19429e1860007a32
Di Bu <dibu@andrew.cmu.edu>
2022-09-24 17:04:31 -0700

    Time: 98 ms (91.63%), Space: 14.1 MB (38.77%) - LeetHub

---
+++ 467-unique-substrings-in-wraparound-string/467-unique-substrings-in-wraparound-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findSubstringInWraproundString(self, p: str) -> int:
+        res = {i: 1 for i in p}
+        l = 1
+        for i, j in zip(p, p[1:]):
+            l = l + 1 if (ord(j) - ord(i)) % 26 == 1 else 1
+            res[j] = max(res[j], l)
+        return sum(res.values())
