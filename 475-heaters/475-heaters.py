1a4376947a3ee5805599086e54014ab6bbb10b61
Di Bu <dibu@andrew.cmu.edu>
2022-09-24 18:17:21 -0700

    Time: 308 ms (91.78%), Space: 17.6 MB (70.56%) - LeetHub

---
+++ 475-heaters/475-heaters.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        heaters = sorted(heaters) + [float('inf')]
+        i = r = 0
+        for x in sorted(houses):
+            while x >= sum(heaters[i:i+2]) / 2.:
+                i += 1
+            r = max(r, abs(heaters[i] - x))
+        return r
