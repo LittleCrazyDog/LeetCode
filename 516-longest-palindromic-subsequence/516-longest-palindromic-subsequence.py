0480a733c086de3ea54b610050caaec2942eb7f3
Di Bu <dibu@andrew.cmu.edu>
2022-09-26 14:24:22 -0700

    Time: 1807 ms (80.89%), Space: 14 MB (95.98%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -1,24 +1,22 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        # Bottom Up DP
-        # Let dp[l][r] denote the length of the longest palindromic subsequence of s[l..r].
-        # There are 2 options:
-        #   - If s[l] == s[r] then dp[l][r] = dp[l+1][r-1] + 2
-        #   - Elif s[l] != s[r] then dp[l][r] = max(dp[l+1][r], dp[l][r-1]).
-        # Then dp[0][n-1] is our result
+        # Bottom Up DP (Space Optimized)
+        # Since we need to access up to 2 dp states: current dp and previous dp state dpPrev
+        # So we can optimize to achieve O(N) in Space Complexity
         # Complexity:
         # Time: O(N^2), where N <= 1000 is the length of s
-        # Space: O(N^2)
+        # Space: O(N)
         
         n = len(s)
-        dp = [[0] * n for _ in range(n)]
+        dp, dpPrev = [0] * n, [0] * n
         
         for i in range(n-1, -1, -1):
-            dp[i][i] = 1
+            dp[i] = 1
             for j in range(i+1, n):
                 if s[i] == s[j]:
-                    dp[i][j] = dp[i+1][j-1] + 2
+                    dp[j] = dpPrev[j-1] + 2
                 else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+                    dp[j] = max(dpPrev[j], dp[j-1])
+            dp, dpPrev = dpPrev, dp
         
-        return dp[0][n-1]
+        return dpPrev[n-1]
