31fa4fcb077002884c256b8483635fff5d95e0ae
Di Bu <dibu@andrew.cmu.edu>
2022-09-26 00:03:10 -0700

    Time: 100 ms (63.02%), Space: 15.4 MB (58.57%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.py
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -5,34 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Memorization of path sum
-    def pathSum(self, root: Optional[TreeNode], target: int) -> int:
-        # define global result and path
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         self.result = 0
-        cache = {0: 1}
-        
-        # recursive to get result
-        self.dfs(root, target, 0, cache)
-        
-        # return result
+        oldPaths = defaultdict(int)
+        oldPaths[0] = 1
+        self.dfs(root, targetSum, 0, oldPaths)
         return self.result
     
-    def dfs(self, root, target, currPathSum, cache):
+    def dfs(self, root, targetSum, currPathSum, oldPaths):
         # exit condition
-        if root is None:
+        if root == None:
             return
         
-        # calculate currPathSum and required oldPathSum
         currPathSum += root.val
-        oldPathSum = currPathSum - target
+        oldPathWanted = currPathSum - targetSum
         
-        # update result and cache
-        self.result += cache.get(oldPathSum, 0)
-        cache[currPathSum] = cache.get(currPathSum, 0) + 1
+        self.result += oldPaths[oldPathWanted]
+        oldPaths[currPathSum] = oldPaths[currPathSum] + 1
         
-        # dfs breakdown
-        self.dfs(root.left, target, currPathSum, cache)
-        self.dfs(root.right, target, currPathSum, cache)
+        # dfs children
+        self.dfs(root.left, targetSum, currPathSum, oldPaths)
+        self.dfs(root.right, targetSum, currPathSum, oldPaths)
         
-        # when move to a different branch, the currPathSum is no longer available, hence remove one.
-        cache[currPathSum] -= 1
+        oldPaths[currPathSum] -= 1
