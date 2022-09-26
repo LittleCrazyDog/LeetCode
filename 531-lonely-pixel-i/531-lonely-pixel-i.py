15476462142f2766f86d7f437d925476a9543fde
Di Bu <dibu@andrew.cmu.edu>
2022-09-26 16:17:22 -0700

    Time: 535 ms (84.87%), Space: 14.9 MB (90.27%) - LeetHub

--- 531-lonely-pixel-i/531-lonely-pixel-i.py
+++ 531-lonely-pixel-i/531-lonely-pixel-i.py
@@ -1,3 +1,15 @@
 class Solution:
     def findLonelyPixel(self, picture: List[List[str]]) -> int:
-        return sum(col.count('B') == 1 == picture[col.index('B')].count('B') for col in zip(*picture))
+        m, n = len(picture), len(picture[0])
+        res = 0
+        for j in range(n):
+            found = False
+            for i in range(m):
+                if picture[i][j] == 'B':
+                    if found or picture[i].count('B') != 1:
+                        found = False
+                        break
+                    found = True
+            if found:
+                res += 1
+        return res
