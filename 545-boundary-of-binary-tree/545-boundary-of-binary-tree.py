9a49cd4094104404f02ce95982bb3301c8d35eae
Di Bu <dibu@andrew.cmu.edu>
2022-10-01 16:13:15 -0700

    Time: 106 ms (8.47%), Space: 16.4 MB (28.49%) - LeetHub

---
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        def dfs_leftmost(node):
+            if not node or not node.left and not node.right:
+                return
+            boundary.append(node.val)
+            if node.left:
+                dfs_leftmost(node.left)
+            else:
+                dfs_leftmost(node.right)
+        
+        def dfs_leaves(node):
+            if not node:
+                return
+            dfs_leaves(node.left)
+            if node != root and not node.left and not node.right:
+                boundary.append(node.val)
+            dfs_leaves(node.right)
+        
+        def dfs_rightmost(node):
+            if not node or not node.left and not node.right:
+                return
+            if node.right:
+                dfs_rightmost(node.right)
+            else:
+                dfs_rightmost(node.left)
+            boundary.append(node.val)
+        
+        if not root:
+            return []
+        
+        boundary = [root.val]
+        dfs_leftmost(root.left)
+        dfs_leaves(root)
+        dfs_rightmost(root.right)
+        return boundary
