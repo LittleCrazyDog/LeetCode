fcf7b0079a9e734a6de66dd32b8783b52ef95988
Di Bu <dibu@andrew.cmu.edu>
2022-10-02 12:46:33 -0700

    Time: 577 ms (39.75%), Space: 13.9 MB (97.63%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,12 +1,18 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        # Solution I - Memoization - Top Down
+        # Solution II - Tabulation - Bottom Up
+        if len(word1) > len(word2):
+            word2, word1 = word1, word2
+            
         m, n = len(word1), len(word2)
-        @cache
-        def lcs(i, j):  # find longest common subsequence
-            if i == m or j == n:
-                return 0
-            return 1 + lcs(i+1, j+1) if word1[i] == word2[j] else max(lcs(i+1, j), lcs(i,j+1))
-        # substract the lcs length from both the strings
-        # the difference is the number of characters that has no deleted.
-        return m + n - 2*lcs(0, 0)
+        prev = [0] * (m + 1)
+        
+        for i in range(n-1, -1, -1):
+            curr = [0] * (m + 1)
+            for j in range(m-1, -1, -1):
+                if word1[j] == word2[i]:
+                    curr[j] = 1 + prev[j+1]
+                else:
+                    curr[j] = max(curr[j+1], prev[j])
+            prev = curr
+        return m + n - 2*prev[0]
