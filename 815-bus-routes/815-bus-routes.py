6c0eb3648fbaa33ec3f2e5b153799b951c1ef78a
Di Bu <dibu@andrew.cmu.edu>
2022-10-02 00:28:22 -0700

    Time: 496 ms (91.37%), Space: 56.8 MB (6.59%) - LeetHub

--- 815-bus-routes/815-bus-routes.py
+++ 815-bus-routes/815-bus-routes.py
@@ -1,20 +1,20 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], S: int, T: int) -> int:
         to_routes = defaultdict(set)
-        for i, route in enumerate(routes):
-            for j in route:
-                to_routes[j].add(i)
+        for bus, route in enumerate(routes):
+            for stop in route:
+                to_routes[stop].add(bus)
         
-        bfs = [(S, 0)]
+        queue = [(S, 0)]
         seen = set([S])
         
-        for stop, bus in bfs:
-            if stop == T: return bus
-            for i in to_routes[stop]:
-                for j in routes[i]:
-                    if j not in seen:
-                        bfs.append((j, bus + 1))
-                        seen.add(j)
-                routes[i] = []  # seen route
+        for curr, dist in queue:
+            if curr == T: return dist
+            for bus in to_routes[curr]:
+                for next in routes[bus]:
+                    if next not in seen:
+                        queue.append((next, dist + 1))
+                        seen.add(next)
+                routes[bus] = []
         
         return -1
