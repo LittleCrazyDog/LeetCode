f1e9621c766e251d277b6284a9fd5a7c2c50ab55
Di Bu <dibu@andrew.cmu.edu>
2022-10-03 23:45:58 -0700

    Time: 844 ms (23.44%), Space: 14.6 MB (100.00%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
@@ -4,42 +4,24 @@ class Solution:
         preSum = [[0] * (n+1) for _ in range(m+1)]
         for r in range(m-1, -1, -1):
             for c in range(n-1, -1, -1):
-                preSum[r][c] = preSum[r][c+1] + preSum[r+1][c] - preSum[r+1][c+1] + (pizza[r][c]=='A')
+                preSum[r][c] = preSum[r][c+1] + preSum[r+1][c] - preSum[r+1][c+1] + (pizza[r][c] == 'A')
         dp = [[[-1 for _ in range(n)] for _ in range(m)] for _ in range(k)]
         return self.dfs(m, n, k-1, 0, 0, dp, preSum)
-        
     
     def dfs(self, m, n, k, r, c, dp, preSum):
         if preSum[r][c] == 0: return 0
         if k == 0: return 1
         if dp[k][r][c] != -1: return dp[k][r][c]
-        ans = 0
         
+        ans = 0
         # cut in horizontal
         for nr in range(r+1, m):
-            if preSum[r][c] - preSum[nr][c] > 0:
+            if preSum[r][c] > preSum[nr][c]:
                 ans = (ans + self.dfs(m, n, k-1, nr, c, dp, preSum)) % (10**9+7)
         # cut in vertical
         for nc in range(c+1, n):
-            if preSum[r][c] - preSum[r][nc] > 0:
+            if preSum[r][c] > preSum[r][nc]:
                 ans = (ans + self.dfs(m, n, k-1, r, nc, dp, preSum)) % (10**9+7)
+        
         dp[k][r][c] = ans
-        
         return ans
-    
-#         @lru_cache(None)
-#         def dp(k, r, c):
-#             if preSum[r][c] == 0: return 0
-#             if k == 0: return 1
-#             ans = 0
-#             # cut horizontally
-#             for nr in range(r+1, m):
-#                 if preSum[r][c] - preSum[nr][c] > 0:
-#                     ans = (ans + dp(k-1, nr, c)) % MOD
-#             # cut vertically
-#             for nc in range(c+1, n):
-#                 if preSum[r][c] - preSum[r][nc] > 0:
-#                     ans = (ans + dp(k-1, r, nc)) % MOD
-#             return ans
-        
-#         return dp(k-1, 0, 0)
