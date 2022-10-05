d5e7efa7e61df03c87514180196ae30aa0fc2422
Di Bu <dibu@andrew.cmu.edu>
2022-10-05 01:14:53 -0700

    Time: 915 ms (13.44%), Space: 16.3 MB (91.27%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,7 +1,5 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
         count = 0
         visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
         for i in range(len(grid)):
