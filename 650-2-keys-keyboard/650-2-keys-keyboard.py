6a6a702c1f871e29baa6a17beeedae149919685b
Di Bu <dibu@andrew.cmu.edu>
2022-10-05 00:50:22 -0700

    Time: 59 ms (71.08%), Space: 14 MB (33.05%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.py
+++ 650-2-keys-keyboard/650-2-keys-keyboard.py
@@ -1,12 +1,6 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        def factors(n):
-            d = 2
-            while d * d <= n:
-                while n % d == 0:
-                    n //= d
-                    yield d
-                d += 1
-            if n > 1:
-                yield n
-        return sum(factors(n))
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return self.minSteps(n // i) + i
+        return 0 if n == 1 else n
