60d486e218ced7a26d2b135dedc621e8cfc08a86
Di Bu <dibu@andrew.cmu.edu>
2022-10-05 14:14:41 -0700

    Time: 522 ms (7.78%), Space: 15.3 MB (54.52%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.py
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -1,9 +1,15 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        one, two = nums[:], nums[:]
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i+1]:
-                one[i] = nums[i+1]
-                two[i+1] = nums[i]
-                break
-        return one == sorted(one) or two == sorted(two)
+        # Greedy
+        # find i with nums[i-1] > nums[i]
+        # modify nums[i-1] or nums[i], e.g, [3,4,2,3]
+        
+        cnt = 0
+        for i in range(1, len(nums)):
+            if nums[i-1] > nums[i]:
+                cnt += 1
+                if i < 2 or nums[i-2] <= nums[i]:
+                    nums[i-1] = nums[i]
+                else:
+                    nums[i] = nums[i-1]
+        return cnt <= 1
