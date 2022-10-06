08d8157d63ef02077363e90759b70b738cd95b55
Di Bu <dibu@andrew.cmu.edu>
2022-10-05 21:08:13 -0700

    Time: 1212 ms (83.88%), Space: 14.2 MB (72.15%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
@@ -3,15 +3,17 @@ class Solution:
         # dp stores two things:
         # 1. the Length of longest subsequence ending at this index
         # 2. Number of longest subsequence that end at this index
-        dp, longest = [[1, 0] for _ in range(len(nums))], 1
+        dp = [[1, 1] for _ in range(len(nums))]
+        max_for_all = 1
         for i, num in enumerate(nums):
-            curr_longest, count = 1, 0
+            max_len, count = 1, 0
             for j in range(i):
                 if nums[j] < num:
-                    curr_longest = max(curr_longest, dp[j][0] + 1)
-            for j in range(i):
-                if dp[j][0] == curr_longest - 1 and nums[j] < num:
-                    count += dp[j][1]
-            dp[i] = [curr_longest, max(count, 1)]
-            longest = max(curr_longest, longest)
-        return sum([item[1] for item in dp if item[0] == longest])
+                    if dp[j][0] + 1 > max_len:
+                        max_len = dp[j][0] + 1
+                        count = 0
+                    if dp[j][0] + 1 == max_len:
+                        count += dp[j][1]
+            dp[i] = [max_len, max(count, dp[i][1])]
+            max_for_all = max(max_for_all, max_len)
+        return sum([item[1] for item in dp if item[0] == max_for_all])
