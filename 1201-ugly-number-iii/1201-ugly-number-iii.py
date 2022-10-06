c477617dbc60545aff05d9185989d9688c3158d5
Di Bu <dibu@andrew.cmu.edu>
2022-10-06 15:23:44 -0700

    Time: 63 ms (26.08%), Space: 14.1 MB (6.33%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.py
+++ 1201-ugly-number-iii/1201-ugly-number-iii.py
@@ -1,13 +1,12 @@
 class Solution:
     def nthUglyNumber(self, n: int, a: int, b: int, c: int) -> int:
+        def ok(x):
+            return x // a + x // b + x // c - x // lcm(a, b) - x // lcm(a, c) - x // lcm(b, c) + x // lcm(a, b, c) >= n
         l, r = 1, 2*10**9+1
         while l < r:
             mid = (l + r) // 2
-            if self.ok(a, b, c, n, mid):
+            if ok(mid):
                 r = mid
             else:
                 l = mid + 1
         return l
-    
-    def ok(self, a, b, c, n, x):
-        return x // a + x // b + x // c - x // lcm(a, b) - x // lcm(a, c) - x // lcm(b, c) + x // lcm(a, b, c) >= n
