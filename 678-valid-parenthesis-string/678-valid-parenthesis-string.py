655981fe4a8b6d30b4af07385f96eb28b89f1bab
Di Bu <dibu@andrew.cmu.edu>
2022-10-05 21:40:11 -0700

    Time: 66 ms (20.69%), Space: 13.7 MB (97.52%) - LeetHub

---
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        cmin = cmax = 0
+        for ch in s:
+            cmax = cmax - 1 if ch == ')' else cmax + 1
+            cmin = cmin + 1 if ch == '(' else max(cmin - 1, 0)
+            if cmax < 0: return False
+        return cmin == 0
