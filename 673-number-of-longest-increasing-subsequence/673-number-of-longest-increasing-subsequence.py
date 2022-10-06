3bd0bd0e4cd589ed03f0e9118fe4f1419523a84e
Di Bu <dibu@andrew.cmu.edu>
2022-10-05 21:04:26 -0700

    Time: 1911 ms (62.24%), Space: 14.4 MB (27.32%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
@@ -12,6 +12,6 @@ class Solution:
             for j in range(i):
                 if dp[j][0] == curr_longest - 1 and nums[j] < num:
                     count += dp[j][1]
-            dp[i] = [curr_longest, max(count, dp[i][1])]
+            dp[i] = [curr_longest, max(count, 1)]
             longest = max(curr_longest, longest)
         return sum([item[1] for item in dp if item[0] == longest])
