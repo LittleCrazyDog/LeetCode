4a263655f880e25f395a5c969cce0f7c2d406e9a
Di Bu <dibu@andrew.cmu.edu>
2022-10-07 01:08:37 -0700

    Time: 41 ms (90.98%), Space: 14.1 MB (93.86%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,9 +1,11 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res, pos = [[]], {}
-        for n in nums:
-            start, l = pos.get(n, 0), len(res)
-            res += [res[i] + [n] for i in range(start, len(res))]
-            pos[n] = l
+        res = []
+        def backtrack(path, idx):
+            res.append(path)
+            for i in range(idx, len(nums)):
+                if i > idx and nums[i] == nums[i-1]: continue
+                backtrack(path+[nums[i]], i+1)
+        backtrack([], 0)
         return res
