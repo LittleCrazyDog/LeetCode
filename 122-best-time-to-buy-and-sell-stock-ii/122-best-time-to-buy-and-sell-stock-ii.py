e8b6244b8354507e47ac15f7f190949d6c9a1cb9
Di Bu <dibu@andrew.cmu.edu>
2022-10-07 18:14:04 -0700

    Time: 81 ms (79.44%), Space: 15.1 MB (68.91%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices or len(prices) is 1:
+            return 0
+        
+        profit = 0
+        
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        
+        return profit
