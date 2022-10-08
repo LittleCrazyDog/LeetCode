5747457953ed2bbb48bfaf8e4cca12d201c38a33
Di Bu <dibu@andrew.cmu.edu>
2022-10-08 00:01:19 -0700

    Time: 2513 ms (39.95%), Space: 52.5 MB (5.01%) - LeetHub

---
+++ 164-maximum-gap/164-maximum-gap.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        # Bucket sort
+        # 1. Distribute numbers to buckets.
+        # 2. Find minimum and maximum in each buckets
+        # 3. Iterate through buckets and check every difference between smallest element in one bucket
+        #    and biggest in previous. Note, that here we also will check differences between say 1-st
+        #    and 3-rd buckets if we have empty 2-nd bucket
+        
+        lo, hi, n = min(nums), max(nums), len(nums)
+        if n <= 2 or hi == lo: return hi - lo
+        B = defaultdict(list)
+        for num in nums:
+            ind = n-2 if num == hi else (num-lo)*(n-1)//(hi-lo)
+            B[ind].append(num)
+        cands = [[min(B[i]), max(B[i])] for i in range(n-1) if B[i]]
+        return max(y[0]-x[1] for x, y in zip(cands, cands[1:]))
