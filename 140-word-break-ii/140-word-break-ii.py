5e7b399e6ec7fad3e87acdc3bc096ff4d1565381
Di Bu <dibu@andrew.cmu.edu>
2022-10-07 23:52:26 -0700

    Time: 52 ms (52.57%), Space: 13.8 MB (81.86%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,30 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        # memo = {len(s): ['']}
-        # def sentences(i):
-        #     if i not in memo:
-        #         memo[i] = [s[i:j] + (tail and ' ' + tail)
-        #                   for j in range(i+1, len(s)+1)
-        #                   if s[i:j] in wordDict
-        #                   for tail in sentences(j)]
-        #     return memo[i]
-        # return sentences(0)
-        return self.helper(s, wordDict, {})
-    
-    def helper(self, s, wordDict, memo):
-        if s in memo: return memo[s]
-        if not s: return []
+        wordSet = set(wordDict)
+        memo = defaultdict(list)
         
-        res = []
-        for word in wordDict:
-            if not s.startswith(word):
-                continue
-            if len(word) == len(s):
-                res.append(word)
-            else:
-                resultOfTheRest = self.helper(s[len(word):], wordDict, memo)
-                for item in resultOfTheRest:
-                    item = word + ' ' + item
-                    res.append(item)
-        memo[s] = res
-        return res
+        def wordbreak_topdown(s):
+            if not s: return [[]]
+            if s in memo: return memo[s]
+            for endIndex in range(1, len(s)+1):
+                word = s[:endIndex]
+                if word in wordDict:
+                    for subsentence in wordbreak_topdown(s[endIndex:]):
+                        memo[s].append([word]+subsentence)
+            return memo[s]
+        
+        wordbreak_topdown(s)
+        return [' '.join(words) for words in memo[s]]
