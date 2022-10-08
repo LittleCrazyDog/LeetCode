a012e385525078a1a0e7fe5da46ff5777860aeb1
Di Bu <dibu@andrew.cmu.edu>
2022-10-08 14:25:22 -0700

    Time: 91 ms (52.57%), Space: 16.2 MB (90.59%) - LeetHub

--- 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.py
+++ 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.py
@@ -6,45 +6,22 @@
 #         self.right = right
 class Solution:
     def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
-        res = []
-        pre_stack = []
-        suc_stack = []
+        res = deque()
+        stack = []
+        run = root
         
-        while root:
-            if root.val < target:
-                pre_stack.append(root)
-                root = root.right
+        while run or stack:
+            if run:
+                stack.append(run)
+                run = run.left
             else:
-                suc_stack.append(root)
-                root = root.left
-        
-        def get_pre(stack):
-            if stack:
-                pre = stack.pop()
-                p = pre.left
-                while p:
-                    stack.append(p)
-                    p = p.right
-                return pre
-        
-        def get_suc(stack):
-            if stack:
-                suc = stack.pop()
-                p = suc.right
-                while p:
-                    stack.append(p)
-                    p = p.left
-                return suc
-        
-        pre = get_pre(pre_stack)
-        suc = get_suc(suc_stack)
-        
-        while k:
-            k -= 1
-            if (pre and not suc) or (pre and suc and abs(pre.val-target) <= abs(suc.val-target)):
-                res.append(pre.val)
-                pre = get_pre(pre_stack)
-            else:
-                res.append(suc.val)
-                suc = get_suc(suc_stack)
+                node = stack.pop()
+                if len(res) < k:
+                    res.append(node.val)
+                elif node.val-target<target-res[0]:
+                    res.popleft()
+                    res.append(node.val)
+                else:
+                    return res
+                run = node.right
         return res
