b189940d58936c0807ebf361428985f838590f24
Di Bu <dibu@andrew.cmu.edu>
2022-10-08 00:02:29 -0700

    Time: 1394 ms (85.42%), Space: 26.9 MB (97.42%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.py
+++ 164-maximum-gap/164-maximum-gap.py
@@ -7,11 +7,17 @@ class Solution:
         #    and biggest in previous. Note, that here we also will check differences between say 1-st
         #    and 3-rd buckets if we have empty 2-nd bucket
         
-        lo, hi, n = min(nums), max(nums), len(nums)
-        if n <= 2 or hi == lo: return hi - lo
-        B = defaultdict(list)
-        for num in nums:
-            ind = n-2 if num == hi else (num-lo)*(n-1)//(hi-lo)
-            B[ind].append(num)
-        cands = [[min(B[i]), max(B[i])] for i in range(n-1) if B[i]]
-        return max(y[0]-x[1] for x, y in zip(cands, cands[1:]))
+        # lo, hi, n = min(nums), max(nums), len(nums)
+        # if n <= 2 or hi == lo: return hi - lo
+        # B = defaultdict(list)
+        # for num in nums:
+        #     ind = n-2 if num == hi else (num-lo)*(n-1)//(hi-lo)
+        #     B[ind].append(num)
+        # cands = [[min(B[i]), max(B[i])] for i in range(n-1) if B[i]]
+        # return max(y[0]-x[1] for x, y in zip(cands, cands[1:]))
+        max_num = 0
+        nums = sorted(nums)
+        for i in range(len(nums)-1):
+            if nums[i+1] - nums[i] > max_num:
+                max_num = nums[i+1] - nums[i]
+        return max_num
