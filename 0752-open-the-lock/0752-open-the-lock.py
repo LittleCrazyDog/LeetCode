7602187959b1798d063d4b972c9e4e61f55b1b12
Di Bu <dibu@andrew.cmu.edu>
2022-10-12 17:54:57 -0700

    Time: 965 ms (69.64%), Space: 15.8 MB (16.50%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,22 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        marker, depth = 'x', -1
+        visited, q = set(deadends), deque(['0000'])
+        
+        while q:
+            depth += 1
+            for _ in range(len(q)):
+                code = q.popleft()
+                if code == target: return depth
+                if code in visited: continue
+                visited.add(code)
+                q.extend(self.successors(code))
+        return -1
+    
+    def successors(self, code):
+        res = []
+        for i, ch in enumerate(code):
+            num = int(ch)
+            res.append(code[:i] + str((num - 1) % 10) + code[i+1:])
+            res.append(code[:i] + str((num + 1) % 10) + code[i+1:])
+        return res
