819ad03a364971f7a65917b7a0d50a62fc09822f
Di Bu <dibu@andrew.cmu.edu>
2022-10-13 14:35:20 -0700

    Time: 547 ms (22.97%), Space: 30.4 MB (45.74%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -0,0 +1,109 @@
+class Node:
+    
+    def __init__(self):
+        self.key_set = set([])
+        self.prev, self.next = None, None
+    
+    def add_key(self, key):
+        self.key_set.add(key)
+    
+    def remove_key(self, key):
+        self.key_set.remove(key)
+    
+    def get_any_key(self):
+        if self.key_set:
+            result = self.key_set.pop()
+            self.add_key(result)
+            return result
+        else:
+            return None
+    
+    def count(self):
+        return len(self.key_set)
+    
+    def is_empty(self):
+        return len(self.key_set) == 0
+
+class DoubleLinkedList:
+    
+    def __init__(self):
+        self.head_node, self.tail_node = Node(), Node()
+        self.head_node.next = self.tail_node
+        self.tail_node.prev = self.head_node
+        return
+    
+    def insert_after(self, x):
+        node, tmp = Node(), x.next
+        x.next, node.prev = node, x
+        node.next, tmp.prev = tmp, node
+        return node
+    
+    def insert_before(self, x):
+        return self.insert_after(x.prev)
+    
+    def remove(self, x):
+        prev_node = x.prev
+        prev_node.next, x.next.prev = x.next, prev_node
+        return
+    
+    def get_head(self):
+        return self.head_node.next
+    
+    def get_tail(self):
+        return self.tail_node.prev
+    
+    def get_sentinel_head(self):
+        return self.head_node
+    
+    def get_sentinel_tail(self):
+        return self.tail_node
+
+class AllOne:
+
+    def __init__(self):
+        self.dll, self.key_counter = DoubleLinkedList(), defaultdict(int)
+        self.node_freq = {0: self.dll.get_sentinel_head()}
+
+    def _rmv_key_pf_node(self, pf, key):
+        node = self.node_freq[pf]
+        node.remove_key(key)
+        if node.is_empty():
+            self.dll.remove(node)
+            self.node_freq.pop(pf)
+        return
+    
+    def inc(self, key: str) -> None:
+        self.key_counter[key] += 1
+        cf, pf = self.key_counter[key], self.key_counter[key]-1
+        if cf not in self.node_freq:
+            # No need to test if pf == 0 since frequency zero points to sentinel node
+            self.node_freq[cf] = self.dll.insert_after(self.node_freq[pf])
+        self.node_freq[cf].add_key(key)
+        if pf > 0:
+            self._rmv_key_pf_node(pf, key)
+
+    def dec(self, key: str) -> None:
+        if key in self.key_counter:
+            self.key_counter[key] -= 1
+            cf, pf = self.key_counter[key], self.key_counter[key]+1
+            if self.key_counter[key] == 0:
+                self.key_counter.pop(key)
+            if cf != 0:
+                if cf not in self.node_freq:
+                    self.node_freq[cf] = self.dll.insert_before(self.node_freq[pf])
+                self.node_freq[cf].add_key(key)
+            self._rmv_key_pf_node(pf, key)
+
+    def getMaxKey(self) -> str:
+        return self.dll.get_tail().get_any_key() if self.dll.get_tail().count() > 0 else ""
+
+    def getMinKey(self) -> str:
+        return self.dll.get_head().get_any_key() if self.dll.get_tail().count() > 0 else ""
+
+
+# Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
