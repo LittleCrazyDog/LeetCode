96f1c165e3fc0208e5e8f5a7e230b716f2bac920
Di Bu <dibu@andrew.cmu.edu>
2022-10-15 22:52:28 -0700

    Time: 3186 ms (20.92%), Space: 13.9 MB (99.09%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,16 +1,11 @@
 class Solution:
     def minDifficulty(self, A: List[int], d: int) -> int:
         n = len(A)
-        if n < d: return -1
-        
-        @lru_cache(None)
-        def dfs(i, d):
-            if d == 1:
-                return max(A[i:])
-            res, maxd = float('inf'), 0
-            for j in range(i, n-d+1):
-                maxd = max(maxd, A[j])
-                res = min(res, maxd + dfs(j+1,d-1))
-            return res
-        
-        return dfs(0, d)
+        dp = [[float('inf')] * n + [0] for _ in range(d+1)]
+        for d in range(1, d+1):
+            for i in range(n-d+1):
+                maxd = 0
+                for j in range(i, n-d+1):
+                    maxd = max(maxd, A[j])
+                    dp[d][i] = min(dp[d][i], maxd + dp[d-1][j+1])
+        return dp[d][0] if dp[d][0] < float('inf') else -1
