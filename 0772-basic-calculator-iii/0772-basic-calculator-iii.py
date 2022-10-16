b956f5b29aaa93f117785d1d250c186e31c6a084
Di Bu <dibu@andrew.cmu.edu>
2022-10-15 22:20:11 -0700

    Time: 42 ms (92.90%), Space: 14 MB (62.67%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.py
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.py
@@ -1,32 +1,28 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s += '$'
-        def helper(stack, i):
-            num = 0
-            sign = '+'
-            while i < len(s):
-                c = s[i]
-                if c == ' ':
-                    i += 1
-                    continue
-                if c.isdigit():
-                    num = 10*num + int(c)
-                    i += 1
-                elif c == '(':
-                    num, i = helper([], i+1)
+        stack, sign, num = [], '+', 0
+        for i, c in enumerate(s + '+'):
+            if c.isdigit():
+                num = num * 10 + ord(c) - ord('0')
+            elif c == '(':
+                stack.append(sign)
+                stack.append('(')
+                sign = '+'
+            elif c in '+-*/)':
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop()*num)
+                elif sign == '/':
+                    stack.append(int(stack.pop()/num))
+                if c == ')':
+                    num, item = 0, stack.pop()
+                    while item != '(':
+                        num += item
+                        item = stack.pop()
+                    sign = stack.pop()
                 else:
-                    if sign == '+':
-                        stack.append(num)
-                    if sign == '-':
-                        stack.append(-num)
-                    if sign == '*':
-                        stack.append(stack.pop()*num)
-                    if sign == '/':
-                        stack.append(int(stack.pop()/num))
-                    num = 0
-                    i += 1
-                    if c == ')':
-                        return sum(stack), i
-                    sign = c
-            return sum(stack)
-        return helper([], 0)
+                    sign, num = c, 0
+        return sum(stack)
