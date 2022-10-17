b8a4ba3b971ad76cae3d80966918fa162629671f
Di Bu <dibu@andrew.cmu.edu>
2022-10-16 18:08:52 -0700

    Time: 64 ms (87.89%), Space: 16 MB (15.40%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -6,24 +6,14 @@
 #         self.right = right
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        return self.helper(1, n)
-    
-    def helper(self, start, end):
-        if start > end:
-            return [None]
-        
-        all_trees = []
-        
-        for curRootVal in range(start, end+1):
-            all_left_trees = self.helper(start, curRootVal-1)
-            all_right_trees = self.helper(curRootVal+1, end)
-            
-            for left_subtree in all_left_trees:
-                for right_subtree in all_right_trees:
-                    curRoot = TreeNode(curRootVal)
-                    curRoot.left = left_subtree
-                    curRoot.right = right_subtree
-                    
-                    all_trees.append(curRoot)
-        
-        return all_trees
+        def generate(start, end):
+            trees = []
+            for root in range(start, end+1):
+                for left in generate(start, root-1):
+                    for right in generate(root+1, end):
+                        node = TreeNode(root)
+                        node.left = left
+                        node.right = right
+                        trees.append(node)
+            return trees or [None]
+        return generate(1, n)
