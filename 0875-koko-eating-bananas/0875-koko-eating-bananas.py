f812b140db6204ce2029f3e75486c7ee752ddce8
Di Bu <dibu@andrew.cmu.edu>
2022-10-19 11:03:43 -0700

    Time: 1042 ms (48.51%), Space: 15.5 MB (74.46%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -11,20 +11,3 @@ class Solution:
     
     def ok(self, piles, h, k):
         return sum([ceil(pile/k) for pile in piles]) <= h
-        
-        
-        
-        
-#         lo, hi = 1, max(piles)
-        
-#         while lo < hi:
-#             k = (lo + hi) // 2
-#             if self.ok(k, piles, h):
-#                 hi = k
-#             else:
-#                 lo = k+1
-#         return lo
-    
-#     def ok(self, k, piles, h):
-#         hoursNeed = [ceil(pile/k) for pile in piles]
-#         return sum(hoursNeed) <= h
