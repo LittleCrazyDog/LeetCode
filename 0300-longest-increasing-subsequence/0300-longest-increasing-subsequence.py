36ec7935aaaeab37f5ae17b8f9ce7d6357041225
Di Bu <dibu@andrew.cmu.edu>
2022-10-19 10:26:01 -0700

    Time: 170 ms (78.04%), Space: 14.3 MB (45.03%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = (i + j) // 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(size, i+1)
+        return size
