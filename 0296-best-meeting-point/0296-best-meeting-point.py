f8893f8723b819e6c35c4af2f78ce0da798dd6da
Di Bu <dibu@andrew.cmu.edu>
2022-10-24 17:17:04 -0700

    Time: 334 ms (22.93%), Space: 15.6 MB (51.67%) - LeetHub

--- 0296-best-meeting-point/0296-best-meeting-point.py
+++ 0296-best-meeting-point/0296-best-meeting-point.py
@@ -1,7 +1,7 @@
 class Solution:
     def minTotalDistance(self, grid: List[List[int]]) -> int:
         rows, cols = self.get_building_points(grid)
-        x_median, y_median = self.get_median_coordinates(rows, cols)
+        x_median, y_median = rows[len(rows)//2], cols[len(cols)//2]
         res = 0
         for x in rows: res += abs(x-x_median)
         for y in cols: res += abs(y-y_median)
@@ -15,10 +15,5 @@ class Solution:
                 if grid[i][j] == 1:
                     rows.append(i)
                     cols.append(j)
-        return rows, cols
-    
-    def get_median_coordinates(self, rows, cols):
-        rows.sort()
         cols.sort()
-        x_median, y_median = rows[len(rows)//2], cols[len(cols)//2]
-        return x_median, y_median
+        return rows, cols
