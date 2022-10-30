3b0e5095d6c5acd88c1941246b207447ff874902
Di Bu <dibu@andrew.cmu.edu>
2022-10-30 16:57:40 -0700

    Time: 3624 ms (43.27%), Space: 27.7 MB (93.41%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        i = 0
+        nums.sort()
+        for j in range(len(nums)):
+            k += nums[j]
+            if k < nums[j] * (j - i + 1):
+                k -= nums[i]
+                i += 1
+        return j - i + 1
