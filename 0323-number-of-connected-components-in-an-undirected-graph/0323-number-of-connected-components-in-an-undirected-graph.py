7623595f033f073fc6714e54989aec3cfbc6d63b
Di Bu <dibu@andrew.cmu.edu>
2022-10-31 23:10:58 -0700

    Time: 98 ms (98.06%), Space: 16.3 MB (46.60%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -3,12 +3,9 @@ class Solution:
         def find(x):
             if d[x] != x: d[x] = find(d[x])
             return d[x]
-        
         def union(x, y):
             d[find(x)] = find(y)
-        
         d = list(range(n))
         for x, y in edges:
             union(x, y)
-        
         return len(set([find(x) for x in d]))
