207e8d6a592f85cd1a85e6503c6f61af58ad6d9f
Di Bu <dibu@andrew.cmu.edu>
2022-11-08 19:59:09 -0800

    Time: 66 ms (63.91%), Space: 13.9 MB (76.06%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -0,0 +1,7 @@
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        res = 0
+        while len(arr) > 1:
+            i = arr.index(min(arr))
+            res += min(arr[i-1:i] + arr[i+1:i+2]) * arr.pop(i)
+        return res
