30be277cb1dafe124a6e2f85a7f7ea0184cac039
Di Bu <dibu@andrew.cmu.edu>
2023-02-28 23:13:59 -0800

    Time: 1376 ms (67.28%), Space: 51.5 MB (30.81%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,8 +1,11 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        def dfs(i):
-            if manager[i] != -1:
-                informTime[i] += dfs(manager[i])
-                manager[i] = -1
-            return informTime[i]
-        return max(map(dfs, range(n)))
+        G = defaultdict(list)
+        for i in range(n):
+            G[manager[i]].append(i)
+        def dfs(u):
+            res = 0
+            for v in G[u]:
+                res = max(res, dfs(v) + informTime[u])
+            return res
+        return dfs(headID)
