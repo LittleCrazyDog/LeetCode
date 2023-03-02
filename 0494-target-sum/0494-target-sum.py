b0eec76ebde99226952af9fb266ae1317fb37b0e
Di Bu <dibu@andrew.cmu.edu>
2023-03-01 20:05:22 -0800

    Time: 372 ms (50.63%), Space: 14.5 MB (65.26%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        index = len(nums) - 1
+        curr_sum = 0
+        self.memo = {}
+        return self.dp(nums, target, index, curr_sum)
+    
+    def dp(self, nums, target, index, curr_sum):
+        if (index, curr_sum) in self.memo:
+            return self.memo[(index, curr_sum)]
+        
+        # Base cases
+        if index < 0 and curr_sum == target:
+            return 1
+        if index < 0:
+            return 0
+        
+        # Decisions
+        positive = self.dp(nums, target, index-1, curr_sum + nums[index])
+        negative = self.dp(nums, target, index-1, curr_sum - nums[index])
+        
+        self.memo[(index, curr_sum)] = positive + negative
+        return positive + negative
