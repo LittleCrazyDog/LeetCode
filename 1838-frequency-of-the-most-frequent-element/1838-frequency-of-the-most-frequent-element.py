ba4cf02584ed146018ad153727e47d936f1dea1b
Di Bu <dibu@andrew.cmu.edu>
2023-03-02 19:06:44 -0800

    Time: 1274 ms (93.89%), Space: 27.7 MB (91.73%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -8,3 +8,13 @@ class Solution:
                 k -= nums[i]
                 i += 1
         return j - i + 1
+        
+        
+        # i = 0
+        # nums.sort()
+        # for j in range(len(nums)):
+        #     k += nums[j]
+        #     if k < nums[j] * (j - i + 1):
+        #         k -= nums[i]
+        #         i += 1
+        # return j - i + 1
